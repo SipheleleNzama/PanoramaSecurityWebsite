4d0c7c5f258afc8b82b307caad5ae6c58394eb67
Siphelele-Nzama <anelenzama07@gmail.com>
2025-09-12 16:15:22 +0200

    Feat: Interactivity

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ document.addEventListener('DOMContentLoaded', function() {
         setTimeout(function() {
             loader.classList.add('hidden');
             }, 2000);
+    }); 
+
+const sections = document.querySelectorAll('.slide-in');
+
+  const observerOptions = {
+    root: null, // viewport
+    threshold: 0.2 // trigger when 20% of the element is visible
+  };
+
+  const sectionObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+        observer.unobserve(entry.target); // Stop observing once animated
+      }
     });
+  }, observerOptions);
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
 });
-
-
