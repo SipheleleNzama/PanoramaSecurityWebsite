29f2fe3c1e24e3149d78652b301810efce21e608
Siphelele-Nzama <anelenzama07@gmail.com>
2025-09-12 15:53:23 +0200

    Loader

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const navMenu = document.getElementById('main-nav');
     hamburger.addEventListener('click', function() {
         navMenu.classList.toggle('active');
+    }); 
+
+        // New code to handle the pre-loader
+       window.addEventListener('load', function() {
+        const loader = document.getElementById('loader-wrapper');
+        setTimeout(function() {
+            loader.classList.add('hidden');
+            }, 2000);
     });
 });
+
+
