35a07aa48547a6b0df81007c8977f68bc4cc6ed3
Siphelele-Nzama <anelenzama07@gmail.com>
2025-09-12 14:29:18 +0200

    Feat: hamburger menu

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburger = document.querySelector('.hamburger-menu');
+    const navMenu = document.getElementById('main-nav');
+    hamburger.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
