a1c2577a3b8d305df9f69ff2f7b6e75cdda7ab86
Siphelele-Nzama <anelenzama07@gmail.com>
2025-09-15 11:32:41 +0200

    feat: Back to top Button

--- script.js
+++ script.js
@@ -32,4 +32,23 @@ const sections = document.querySelectorAll('.slide-in');
   sections.forEach(section => {
     sectionObserver.observe(section);
   });
+
+   const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 300) {
+            backToTopButton.classList.add('show');
+        } else {
+            backToTopButton.classList.remove('show');
+        }
+    });
+
+    backToTopButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 });
+
